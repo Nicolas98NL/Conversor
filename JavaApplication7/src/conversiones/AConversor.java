b0784ab2604140a111f488727198df2d4972ac41
nicolas <nicolas@Nicolas>
2017-10-17 23:21:21 -0500

    cONVERSOR TOTAL

---
+++ JavaApplication7/src/conversiones/AConversor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversiones;
+
+/**
+ *
+ * @author nicolas
+ */
+public abstract class AConversor {
+    double dato;
+    abstract double grado2radian ();
+    abstract double farenheit2kelvin ();
+}
